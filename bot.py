f80a0525370c80b3c0fb66bfeb32e6e12cd7f3b7
Andy Jiang <ajiang@umich.edu>
2020-07-31 19:57:22 -0400

    init commit
    yep

---
+++ bot.py
@@ -0,0 +1,65 @@
+# bot.py
+import os
+import random
+import json
+import * from helpers.py
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+db = load_players()
+
+teamA = []
+spymaster_A = ''
+teamB = []
+spymaster_B = ''
+game_live = False
+
+
+bot = commands.Bot(command_prefix='^')
+
+@bot.command(name='start', help='Start a game of codenames, give players in space seperated list')
+async def start_game(ctx, arg):
+    if game_live:
+        await ctx.send("Game still live, cancel or give winner with !finish")
+
+    players = arg.split(' ')
+    random.shuffle(players)
+
+    teamA = players[:len(players)/2]
+    teamB = players[len(players/2):]
+
+    teamA_spymaster = get_team_spymaster(teamA, db)
+    teamB_spymaster = get_team_spymaster(teamB, db)
+
+
+    response = "Starting game...\nTeam A: {} Spymaster: {}\n Team B: Spymaster: {}"
+    response.format(', '.join(teamA), teamA_spymaster, ', '.join(teamB), teamB_spymaster)
+    game_live = True
+    await ctx.send(response)
+
+@bot.command(name='finish', help='End the current game of codenames, give winner "A" or "B" or "cancel"')
+async def start_game(ctx, arg):
+    if not game_live:
+        await ctx.send("No game live, nothing done")
+    if arg == 'cancel':
+        game_live = False
+        response = "Current game cancelled. Game details\n Team A: {} Spymaster: {}\n Team B: Spymaster: {}"
+        response.format(', '.join(teamA), teamA_spymaster, ', '.join(teamB), teamB_spymaster)
+        await ctx.send(response)
+    else:
+        if arg == A:
+            finish_game(teamA, teamA_spymaster, teamB, teamB_spymaster, db)
+        else:
+            finish_game(teamB, teamB_spymaster, teamA, teamA_spymaster, db)
+        with open('data.json', 'w') as outfile:
+            json.dump(db, outfile)
+        game_live = False
+        response = "Congrats to Team {}, scores have been updated"
+        response.format(arg)
+        await ctx.send(response)
+
+bot.run(TOKEN)
+
---
+++ helpers.py
@@ -0,0 +1,28 @@
+def load_players():
+    with open('data.json') as f:
+        db = json.load(f)
+
+def get_team_spymaster(team, db):
+    current_spymaster = ""
+    games_since = 9999
+    for player in team:
+        if db['player']['spymaster'] < games_since:
+            current_spymaster = player
+            games_since = db['player']['spymaster']
+    return current_spymaster
+
+def finish_game(winner, winner_spy, loser, loser_spy, db):
+    for player in winner:
+        db[player]['w'] += 1
+        if player == winner_spy:
+            db[player]['spymaster'] = 0
+        else:
+            db[player]['spymaster'] += 1
+    for player in loser:
+        db[player]['l'] += 1
+        if player == loser_spy:
+            db[player]['spymaster'] = 0
+        else:
+            db[player]['spymaster'] += 1
+
+
