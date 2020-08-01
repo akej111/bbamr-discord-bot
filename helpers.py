9bdf9c05062db57ca3784b0a84b950dc2541b15c
Andy Jiang <ajiang@umich.edu>
2020-07-31 20:28:05 -0400

    test

--- helpers.py
+++ helpers.py
@@ -8,9 +8,9 @@ def get_team_spymaster(team, db):
     current_spymaster = ""
     games_since = 9999
     for player in team:
-        if db['player']['spymaster'] < games_since:
+        if db[player]['spymaster'] < games_since:
             current_spymaster = player
-            games_since = db['player']['spymaster']
+            games_since = db[player]['spymaster']
     return current_spymaster
 
 def finish_game(winner, winner_spy, loser, loser_spy, db):
